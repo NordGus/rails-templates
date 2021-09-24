ff06e1eed6aa6474d0f7978e425f03de9c50a630
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-09-24 15:48:09 +0200

    updating the template

--- basic_docker.rb
+++ basic_docker.rb
@@ -1,15 +1,10 @@
-ruby_ver_def = '3.0.0'
-pg_ver_def = '13'
-redis_ver_def = '6'
-
-ruby_ver = ask("Ruby version ?")
+ruby_ver = ask("Ruby version?")
 pg_ver = ask("PostgreSQL version?")
 redis_ver = ask("Redis version?")
 
-ruby_ver = ruby_ver_def unless ruby_ver.present?
-pg_ver = pg_ver_def unless pg_ver.present?
-redis_ver = redis_ver_def unless redis_ver.present?
-
+abort 'Ruby version is required' unless ruby_ver.present?
+abort 'PostgreSQL version is required' unless pg_ver.present?
+abort 'Redis version is required' unless redis_ver.present?
 
 file '.dockerignore', <<-CODE
 .git
@@ -30,8 +25,6 @@ README.md
 !/storage/.keep
 .byebug_history
 .idea/
-/config/credentials/*
-!/config/credentials/.keep
 /public/*
 !/public/robots.txt
 CODE
@@ -39,13 +32,17 @@ CODE
 file 'Dockerfile', <<-CODE
 FROM ruby:#{ruby_ver}
 
-RUN apt-get update -qq && apt-get install -y postgresql-client
+RUN curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+RUN apt-get update && \
+    apt-get install postgresql-client nano nodejs -y && \
+    npm install --global yarn
 
 WORKDIR /var/app
 COPY Gemfile /var/app/Gemfile
 COPY Gemfile.lock /var/app/Gemfile.lock
 RUN bundle install
 COPY . /var/app
+RUN yarn install
 
 COPY entrypoint.sh /usr/bin/
 RUN chmod +x /usr/bin/entrypoint.sh
@@ -73,37 +70,17 @@ file 'docker-compose.yml', <<-CODE
 version: "3.9"
 services:
   dev:
-    build: .
-    command: bash -c "rm -f tmp/pids/server.pid && bundle exec rails s -p 3000 -b '0.0.0.0'"
-    tty: true
-    stdin_open: true
-    environment:
-      TERM: xterm-256color
-      RAILS_ENV: development
-      REDIS_URL: redis://redis:6379/0
+    build: 
+      context: .
+      name: #{app_name}-app
+    command: tail -f /dev/null
     volumes:
       - .:/var/app
-      - ./config/master.key:/var/app/config/credentials/development.key
-      - ./config/credentials.yml.enc:/var/app/config/credentials/development.yml.enc
+      - /var/app/node_modules
     ports:
       - "3000:3000"
-    depends_on:
-      - db
-      - redis
-
-  test:
-    build: .
-    command: bash -c "rm -f tmp/pids/server.pid && bundle exec rspec -f d"
-    tty: true
-    stdin_open: true
-    environment:
-      TERM: xterm-256color
-      RAILS_ENV: test
-      REDIS_URL: redis://redis:6379/0
-    volumes:
-      - .:/var/app
-      - ./config/master.key:/var/app/config/credentials/test.key
-      - ./config/credentials.yml.enc:/var/app/config/credentials/test.yml.enc
+      - "1234:1234"
+      - "26166:26168"
     depends_on:
       - db
       - redis
@@ -113,6 +90,7 @@ services:
     volumes:
       - #{app_name}-db:/var/lib/postgresql/data
     environment:
+      POSTGRES_USER: #{app_name}
       POSTGRES_PASSWORD: localdev
 
   redis:
@@ -121,44 +99,6 @@ services:
 volumes:
   #{app_name}-db:
 CODE
-
-file 'bin/serve', <<-CODE
-#! /bin/bash
-
-docker-compose run --service-ports dev
-
-docker-compose down
-CODE
-
-file 'bin/test', <<-CODE
-#! /bin/bash
-
-docker-compose run --service-ports test
-
-docker-compose down
-CODE
-
-file 'bin/rebuild', <<-CODE
-#! /bin/bash
-
-docker-compose up --build dev test
-
-docker-compose down
-CODE
-
-file 'bin/console', <<-CODE
-#! /bin/bash
-
-docker-compose run --service-ports dev rails console
-
-docker-compose down
-CODE
-
-inside('bin') do
-  %w(serve test rebuild console).each do |file|
-    run "sudo chmod +x #{file}"
-  end
-end
 
 git add: "."
 git commit: %Q{ -m 'Docker setup' }
